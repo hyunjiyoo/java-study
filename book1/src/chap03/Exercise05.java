1332aa2d0d3c40445fbd159b9f841cadd26f816d
hyunjiyoo <solteee23@gmail.com>
2018-08-10 22:34:45 +0900

    p103-5

--- book1/src/chap03/Exercise05.java
+++ book1/src/chap03/Exercise05.java
@@ -1,9 +1,9 @@
 package chap03;
 
 public class Exercise05 {
-	
+	// value가 356일때, 300이 나올 수 있도록 하시오.
 	public static void main(String[] args) {
 		int value = 356;
-		System.out.println(value - (value % 3));
+		System.out.println(value/100 * 100);
 	}
 }
