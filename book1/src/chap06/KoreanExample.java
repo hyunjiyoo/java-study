bb767853cc70974d02f9342b9c5ef95f7032b59b
hyunjiyoo <solteee23@gmail.com>
2018-08-12 20:27:41 +0900

    chap06-객체 생성후 필드값 출력

---
+++ book1/src/chap06/KoreanExample.java
@@ -0,0 +1,13 @@
+package chap06;
+
+public class KoreanExample {
+    public static void main(String[] args) {
+        Korean k1 = new Korean("박자바", "1010265-5214536");
+        System.out.println("k1.name: " + k1.name);
+        System.out.println("k1.ssn: " + k1.ssn);
+
+        Korean k2 = new Korean("김자바", "1542985-75846953");
+        System.out.println("k2.name: " + k2.name);
+        System.out.println("k2.ssn: " + k2.ssn);
+    }
+}
