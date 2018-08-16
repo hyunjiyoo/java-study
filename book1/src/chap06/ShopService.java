fe7db3699a828cc0a10c9b6f817c04d5ea831051
hyunjiyoo <solteee23@gmail.com>
2018-08-16 12:09:45 +0900

    chap06-Exercise18

---
+++ book1/src/chap06/ShopService.java
@@ -0,0 +1,12 @@
+package chap06;
+
+public class ShopService {
+    // 객체를 하나만 생성 (singleton)
+    private static ShopService singleton = new ShopService();
+
+    private ShopService() { }
+
+    public static ShopService getInstance() {
+        return singleton;
+    }
+}
