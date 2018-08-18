0c760ce190e25dc25cbb58c5dbe63c85baa27339
hyunjiyoo <solteee23@gmail.com>
2018-08-18 11:28:08 +0900

    chap07-자식 객체 이용

--- book1/src/chap07/Student.java
+++ book1/src/chap07/Student.java
@@ -4,7 +4,7 @@ public class Student extends People {
     public int studentNo;
 
     public Student(String name, String ssn, int studentNo) {
-        super (name, ssn); // 부모 생성자 호출
+        super (name, ssn); // 부모 생성자 호출 ( People(String name, String ssn) 을 호출)
         this.studentNo = studentNo;
     }
 }
