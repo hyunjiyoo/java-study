f40e9028267f8b4a1f3ac410a9473b074d682d2a
hyunjiyoo <solteee23@gmail.com>
2018-08-19 12:30:05 +0900

    chap08-인터페이스(구현클래스)

---
+++ book1/src/chap08/RemoteControllerExample.java
@@ -0,0 +1,25 @@
+package chap08;
+// 인터페이스 변수에 구현 객체 대입
+public class RemoteControllerExample {
+    public static void main(String[] args) {
+        // 인터페이스 변수
+        RemoteControl rc;
+        // 구현객체를 변수에 대입
+        rc = new Television();
+        rc = new Audio();
+
+        // 익명 구현 클래스
+        // 인터페이스에 선언된 모든 추상 메소드들의 실체 메소드를 작성해야함.
+        RemoteControl remoteControl = new RemoteControl() {
+            @Override
+            public void turnOn() {
+            }
+            @Override
+            public void turnOff() {
+            }
+            @Override
+            public void setVolume(int volume) {
+            }
+        };
+    }
+}
