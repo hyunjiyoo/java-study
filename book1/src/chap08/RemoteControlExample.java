44f4f6f662b01a86b8e11ea60abc5f6865872b71
hyunjiyoo <solteee23@gmail.com>
2018-08-19 12:46:18 +0900

    chap08-인터페이스 사용

---
+++ book1/src/chap08/RemoteControlExample.java
@@ -0,0 +1,20 @@
+package chap08;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        // 인터페이스 변수 선언
+        RemoteControl rc = null;
+
+        // Television 객체를 인터페이스 타입에 대입
+        rc = new Television();
+        // 인터페이스의 turnOn(), turnOff() 호출
+        rc.turnOn();
+        rc.turnOff();
+
+        // Audio 객체를 인터페이스 타입에 대입
+        rc = new Audio();
+        // 인터페이스의 turnOn(), turnOff() 호출
+        rc.turnOn();
+        rc.turnOff();
+    }
+}
