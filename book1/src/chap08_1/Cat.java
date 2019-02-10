a2d1c28f64c5ecdd5ee49ca0e9a965c97e80b154
hyunjiyoo <solteee23@gmail.com>
2019-02-10 14:23:17 +0900

    190210

---
+++ book1/src/chap08_1/Cat.java
@@ -0,0 +1,8 @@
+package chap08_1;
+
+public class Cat implements Soundable {
+    @Override
+    public String sound() {
+        return "야옹";
+    }
+}
---
+++ book1/src/chap08_1/Dog.java
@@ -0,0 +1,8 @@
+package chap08_1;
+
+public class Dog implements Soundable {
+    @Override
+    public String sound() {
+        return "멍멍";
+    }
+}
